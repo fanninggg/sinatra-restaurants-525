0e7b0aaa38cd19615120b4f2a1331d22904f1b96
Ben Fanning fanninggg@gmail.com <fanninggg@gmail.com>
2021-03-04 21:32:50 +0000

    Sinatra restaurants app for batch 525

--- app.rb
+++ app.rb
@@ -3,6 +3,26 @@ require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
 
+# See all restaurants
 get "/" do
-  "Hello world!"
+	@restaurants = Restaurant.all
+  erb :index
+end
+
+# See detailed information about a single restaurant
+get "/restaurants/:id" do
+	restaurant_id = params["id"]
+	@restaurant = Restaurant.find(restaurant_id)
+	erb :show
+end
+
+# Add a restaurant
+post "/restaurants" do
+	# 1. Take the user input from the params and store it in a variable 
+	name = params["name"]
+	city = params["city"]
+	# 2. Create a new instance of restaurant with that data
+	new_restaurant = Restaurant.create(name: name, city: city)
+	# 3. Redirect them
+	redirect "/restaurants/#{new_restaurant.id}"
 end
